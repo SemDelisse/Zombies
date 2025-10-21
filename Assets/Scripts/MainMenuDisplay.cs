0e17aa7cc1c03a4ade6894f596756f3b093442ab
SemDelisse <semdelisse@gmail.com>
2025-10-21 13:27:51 +0200

    Multiplayer
    You can connect on the same network but the game is not fully synchronized yet. It just spawns the player object when someone new joins.

--- Assets/Scripts/MainMenuDisplay.cs
+++ Assets/Scripts/MainMenuDisplay.cs
@@ -1,10 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
+using UnityEngine.SceneManagement;
 
 public class MainMenuDisplay : MonoBehaviour
 {
     public void StartHost() {
+        NetworkManager.Singleton.StartHost();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
 
+    public void JoinServer() {
+        NetworkManager.Singleton.StartClient();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit() {
+        Application.Quit();
     }
 }
